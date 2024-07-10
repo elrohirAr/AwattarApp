bf1e28b9bfec5ff1ccc09f3534c044a44b1d8a04
elrohirAr <davidhaas24@gmail.com>
2024-07-10 16:48:42 +0200

    Projektdateien hinzufügen.

---
+++ AwattarApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AwattarApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnFetchButtonClicked(object sender, EventArgs e)
+        {
+            resultLabel.Text = "Fetching data...";
+            var json = await FetchMarketDataAsync("https://api.awattar.at/v1/marketdata");
+            resultLabel.Text = json;
+        }
+
+        private async Task<string> FetchMarketDataAsync(string url)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetStringAsync(url);
+                    var formattedJson = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(response), Formatting.Indented);
+                    return formattedJson;
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error fetching data: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ AwattarApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AwattarApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
